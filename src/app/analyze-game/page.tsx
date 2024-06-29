2537612933f9d639b9f4414adc65474bfbdbfef2
Armaan <armaan33000@gmail.com>
2024-06-29 15:37:18 +0530

    welp, even after a lot of debugging, only able to create till this

--- src/app/analyze-game/page.tsx
+++ src/app/analyze-game/page.tsx
@@ -16,6 +16,28 @@ const AnalyzeGamePage = (props: Props) => {
         </div>
         <br />
         <h2 className="text-3xl">Enter your game pgn here </h2>
+        <br />
+        <textarea className="h-72 w-96 border-2 border-black"></textarea>
+        <br />
+        <input
+          type="button"
+          value="Get Analysis!"
+          className="px-4 py-2 bg-black hover:bg-[#171717] transition-all cursor-pointer rounded-md text-white"
+        />
+        <br />
+
+        <div className="analysisDiv border border-1 border-black rounded-md p-[1rem]">
+          <h2>Best Moves: e4, Nf3</h2>
+          <h2>Excellent Moves: e4, Nf3</h2>
+          <h2>Good Moves: e4, Nf3</h2>
+          <h2>Mistakes: e4, Nf3</h2>
+          <h2>Blunders: e4, Nf3</h2>
+        </div>
+        <br />
+
+        <div className="chessBoardRender">
+          {/* I wanna render the chessboard here  */}
+        </div>
       </div>
     </>
   );
